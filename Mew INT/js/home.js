48c81d18bbd3f901a019cc808a3ca5ced619f39f
Tuyen <chimsedilinh@gmail.com>
2020-07-05 16:37:52 +0700

    Tuyenattt gui
    giao diện gửi ngày 5/7/2020

---
+++ Mew INT/js/home.js
@@ -0,0 +1,24 @@
+﻿ $(function(){
+ 	$(window).scroll(function(event) {
+ 		/* Act on the event */
+ 		var vitribody = $(window).scrollTop();
+ 		var vitrihienlen = $('.noidung').offset().top;
+ 		if(vitribody >= vitrihienlen){
+ 			$('.nutlen a.fa.fa-chevron-up').addClass('hienra');
+ 			$('.khoimenu').addClass('thaydoi');
+
+ 		}
+ 		else {
+ 			$('.nutlen a.fa.fa-chevron-up').removeClass('hienra');
+ 			$('.khoimenu').removeClass('thaydoi');
+ 		}
+ 	});
+ 	$('.nutlen').click(function(event) {
+ 		/* Act on the event */
+ 		$('body').animate({'scrollTop':0});
+
+
+ 	});
+ 
+})  
+ 
